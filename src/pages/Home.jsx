f170c94e8cd7590f086a671bb5bb71481ebf093d
ahmed-dev <ahmed.quereshi@inspon.com>
2025-11-20 01:19:27 +0500

    influencer video added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -432,9 +432,8 @@ const Home = () => {
         {/* Moment Section */}
         <section className="moment-section section-padding">
           <div className="container">
-
             <div class="video-container">
-                <iframe width="750" height="400" src="https://www.youtube.com/embed/N1MxFzwigII?si=r7rXb1fNwH4ndYmJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/CSSHUHGZtHA?si=XAJ8jt8UhdVIs3rX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
            
             {/* <h2>Turn a moment into a song</h2>
